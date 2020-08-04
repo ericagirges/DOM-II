8dc7d1f5c6275276c60eb0b242f5ac82e679168e
Erica Girges <ericafiorenza@gmail.com>
2020-08-04 15:47:27 -0700

    7 completed event listeners

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// Your code goes here
+// declare variables
 const navLink = document.querySelectorAll(".nav-link");
 const nav = document.querySelector("nav");
 
@@ -17,6 +17,8 @@ nav.addEventListener("click", function(event){
     event.srcElement.setAttribute("style", "color: #F19804; font-size: 2.0rem;")
 });
 
+
+//declare variables
 const header = document.querySelector(".intro");
 const headerImg = header.querySelector("img");
 
@@ -30,3 +32,48 @@ headerImg.addEventListener("mouseover", function(event){
 headerImg.addEventListener("mouseout", function(event){
     event.target.setAttribute("style", "filter: none;");
 });
+
+const textContent = document.querySelectorAll(".text-content");
+
+//add mouseover change background color and add padding to text-content
+textContent[0].addEventListener("mouseover", function(event){
+    event.target.setAttribute("style", "background:#80DDE3; padding: 10px;")
+});
+
+textContent[1].addEventListener("mouseover", function(event){
+    event.target.setAttribute("style", "background: #FACC7C; padding: 10px;")
+});
+
+// when text is copied send alert
+document.addEventListener("copy", function(event){
+    alert("Sucessfully copied.");
+
+});
+
+// dblclick on sign me up send alert
+const signupButton = document.querySelectorAll(".btn");
+
+signupButton.addEventListener("dblclick", function(event){
+    alert("Your adventure begins here!");
+});
+
+// add action on keydown
+
+const pageImages = document.querySelectorAll(".img-content");
+
+document.addEventListener("keydown", function(event){
+    if(event.key === "Shift"){
+        pageImages[1].src = "https://images.unsplash.com/photo-1524850011238-e3d235c7d4c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1087&q=80";
+    
+    };
+  
+});
+
+
+
+document.addEventListener("select", function(event){
+    event.srcElement.setAttribute("style", "color: #4487FB");
+});
+
+
+
