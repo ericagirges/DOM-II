36c43842f892b6241471642336fa6989b68684ba
Erica Girges <ericafiorenza@gmail.com>
2020-08-04 17:42:57 -0700

    all 10 events completed

--- js/index.js
+++ js/index.js
@@ -2,80 +2,75 @@
 const navLink = document.querySelectorAll(".nav-link");
 const nav = document.querySelector("nav");
 
-
-// mouseover nav link change color blue
-nav.addEventListener("mouseover", function(event){
-    event.target.style.color = "#5EC4FE";
-    setTimeout(function(){
-        event.target.style.color = "";
-    }, 300);
-}, false);
-
 // click nav link change color orange and increase font size
-nav.addEventListener("click", function(event){
-    navLink.forEach(el => el.setAttribute("style", "color: #000; font-size: 1.6rem;"));
-    event.srcElement.setAttribute("style", "color: #F19804; font-size: 2.0rem;")
+nav.addEventListener("click", function (event) {
+  navLink.forEach((el) =>
+    el.setAttribute("style", "color: #000; font-size: 1.6rem;")
+  );
+  event.srcElement.setAttribute("style", "color: #F19804; font-size: 2.0rem;");
 });
-
 
 //declare variables
 const header = document.querySelector(".intro");
 const headerImg = header.querySelector("img");
 
-
 // add mouseover header img to black and white
-headerImg.addEventListener("mouseover", function(event){
-    event.target.setAttribute("style", "filter: grayscale(100%);");
+headerImg.addEventListener("mouseover", function (event) {
+  event.target.setAttribute("style", "filter: grayscale(100%);");
 });
 
 // add mouseout header img back to normal color
-headerImg.addEventListener("mouseout", function(event){
-    event.target.setAttribute("style", "filter: none;");
+headerImg.addEventListener("mouseout", function (event) {
+  event.target.setAttribute("style", "filter: none;");
 });
 
 const textContent = document.querySelectorAll(".text-content");
 
-//add mouseover change background color and add padding to text-content
-textContent[0].addEventListener("mouseover", function(event){
-    event.target.setAttribute("style", "background:#80DDE3; padding: 10px;")
+//add mouseenter and mouseleave change background color and add padding to text-content
+textContent[0].addEventListener("mouseenter", function (event) {
+  event.target.setAttribute("style", "background:#80DDE3; padding: 10px;");
 });
 
-textContent[1].addEventListener("mouseover", function(event){
-    event.target.setAttribute("style", "background: #FACC7C; padding: 10px;")
+textContent[0].addEventListener("mouseleave", function(event){
+    event.target.setAttribute("style", "background: none; padding: nonee;");
+});
+
+
+// mousemove change color and add padding
+textContent[1].addEventListener("mousemove", function (event) {
+  event.target.setAttribute("style", "background: #FACC7C; padding: 10px;");
+});
+
+textContent[1].addEventListener("mouseleave", function(event){
+    event.target.setAttribute("style", "background: none; padding: nonee;");
 });
 
 // when text is copied send alert
-document.addEventListener("copy", function(event){
-    alert("Sucessfully copied.");
-
+document.addEventListener("copy", function (event) {
+  alert("Sucessfully copied.");
 });
 
 // dblclick on sign me up send alert
 const signupButton = document.querySelectorAll(".btn");
 
-Array.from(signupButton).forEach(function(button){
-    button.addEventListener("dblclick", function(event){
-        alert("Your adventure begins here!");
-        });
-    });
+Array.from(signupButton).forEach(function (button) {
+  button.addEventListener("dblclick", function (event) {
+    alert("Your adventure begins here!");
+  });
+});
 
 // add action on keydown
+const pageLogo = document.querySelector("h1");
 
-const pageImages = document.querySelectorAll(".img-content");
-
-document.addEventListener("keydown", function(event){
-    if(event.key === "Shift"){
-        pageImages[1].src = "https://images.unsplash.com/photo-1524850011238-e3d235c7d4c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1087&q=80";
-    
-    };
-  
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Shift") {
+    pageLogo.setAttribute("style", "transform: rotate(0.5turn);");
+  } else {
+    pageLogo.setAttribute("style", "transform: none;");
+  }
 });
 
-
-
-document.addEventListener("select", function(event){
-    event.srcElement.setAttribute("style", "color: #4487FB");
+// on resize of window change color of h1
+window.addEventListener("resize", function (event) {
+  pageLogo.setAttribute("style", "color:#F65915; font-size: 3.6rem;");
 });
-
-
-
